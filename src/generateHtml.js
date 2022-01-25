f6c08d29e916e7b428224c2b76729d3d5d5cdb44
Cesar Sanchez <cesanchezesc@gmail.com>
2022-01-25 12:11:14 -0600

    Finished app

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -8,7 +8,7 @@ const generateHtml = (cards) =>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Team Profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./index.css" />
+    <link rel="stylesheet" href="./style.css" />
 </head>
 <body class="bg-secondary">
     <header class="container-fluid bg-dark text-white" style="height: 100px; text-align: center; padding: 20px 0px 20px 0">
