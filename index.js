57482bf80a0f476412cd3fc8de9471e48bba9582
Cesar Sanchez <cesanchezesc@gmail.com>
2022-01-25 09:13:56 -0600

    finished with the inquirer first version for every employee

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
 
 const listEmployees = []
 
@@ -58,6 +60,7 @@ inquirer
     .then((answers) => {
         const manager = new Manager(answers.name,answers.id,answers.email,answers.officeNo)
         listEmployees.push(manager)
+        console.log(listEmployees)
         employeeMenu();
     })
 }
@@ -68,22 +71,197 @@ inquirer
         {
             name: "menu",
             type: "list",
-            message: "What type of employee do you want to add?",
-            choices: ["Engineer","Intern","Exit"]
+            message: "What type of employee do you want to add, or are you done?",
+            choices: ["Engineer","Intern","Finish"]
         }
             
     ])
     .then((answer) =>{
         if (answer = "Engineer") {
             addEngineer();
-        }
-        if (answer = "Intern") {
+        } else if (answer = "Intern") {
             addIntern();
-        }
-        if (answer = "Exit") {
+        } else if (answer = "Finish") {
             renderTeam();
+        }else {
+            return
         }
     })
 }
+
+const addEngineer = () => {
+
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "Enter the Engineer name?",
+                validate(answer) { if(!answer) {
+                    return "Please, write a name!"
+                }
+                return true
+                }
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "Enter the Engineer id?",
+                validate(answer) { 
+                    valid = /^[0-9]+$/.test(answer)
+                    if(!valid) {
+                    return "Please, write a number!"
+                }
+                return true
+                }
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "Enter the Engineer email?",
+                validate (answer) { 
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(answer)
+                    if (!valid) {
+                    return "Please, write a valid email!"
+                }
+                return true
+                }
+            },
+            {
+                name: "github",
+                type: "input",
+                message: "Enter the Engineer Github username",
+                validate(answer) { if(!answer) {
+                    return "Please, write a name!"
+                }
+                return true
+                }
+            }
+    
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(answers.name,answers.id,answers.email,answers.github)
+            listEmployees.push(engineer)
+            console.log(listEmployees)
+            employeeMenu();
+        })
+    }
+
+const addIntern = () => {
+
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "Enter the Intern name?",
+                validate(answer) { if(!answer) {
+                    return "Please, write a name!"
+                }
+                return true
+                }
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "Enter the Intern id?",
+                validate(answer) { 
+                    valid = /^[0-9]+$/.test(answer)
+                    if(!valid) {
+                    return "Please, write a number!"
+                }
+                return true
+                }
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "Enter the Intern email?",
+                validate (answer) { 
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(answer)
+                    if (!valid) {
+                    return "Please, write a valid email!"
+                }
+                return true
+                }
+            },
+            {
+                name: "school",
+                type: "input",
+                message: "Enter the Intern School name",
+                validate(answer) { if(!answer) {
+                    return "Please, write a name!"
+                }
+                return true
+                }
+            }
+    
+        ])
+        .then((answers) => {
+            const intern = new Intern(answers.name,answers.id,answers.email,answers.school)
+            listEmployees.push(intern)
+            console.log(listEmployees)
+            employeeMenu();
+        })
+    }
+
+const addEngineer = () => {
+
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "Enter the Engineer name?",
+                validate(answer) { if(!answer) {
+                    return "Please, write a name!"
+                }
+                return true
+                }
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "Enter the Engineer id?",
+                validate(answer) { 
+                    valid = /^[0-9]+$/.test(answer)
+                    if(!valid) {
+                    return "Please, write a number!"
+                }
+                return true
+                }
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "Enter the Engineer email?",
+                validate (answer) { 
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(answer)
+                    if (!valid) {
+                    return "Please, write a valid email!"
+                }
+                return true
+                }
+            },
+            {
+                name: "github",
+                type: "input",
+                message: "Enter the Engineer Github username",
+                validate(answer) { if(!answer) {
+                    return "Please, write a name!"
+                }
+                return true
+                }
+            }
+    
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(answers.name,answers.id,answers.email,answers.github)
+            listEmployees.push(engineer)
+            console.log(listEmployees)
+            employeeMenu();
+        })
+    }
+
 
 startApp();
